2b1e17581fcd329de5884a97bbd3a3e86147304a
darco2018 <newsecond@outlook.com>
2019-09-21 13:40:04 +0200

    Find movies by search term - completed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // res.send('Welcome to / route'); // app hangs when commented off
+  res.render('index'); // app hangs when commented off
   // next(); - will continue chain when no view rendering
 });
 
--- routes/movies.js
+++ routes/movies.js
@@ -11,7 +11,7 @@ function Movie(title, year) {
   this.year = year;
 }
 // const inMemoryMovieDb = [new Movie('Film 1', '1923'), new Movie('Film 2', '1923'), new Movie('Film 3', '1923')];
-
+/* GET /movies page. */
 router.get('/', (req, res) => {
   const queryParams = 's=polish'; // !!!
   const url = urlBase + queryParams;
@@ -26,6 +26,27 @@ router.get('/', (req, res) => {
     res.render('movies', { movies: moviesObj });
   });
   // res.render('movies', { movies: inMemoryMovieDb });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  const search = req.body.searchword;
+  const queryParams = !search ? 's=polish' : `s=${search}`; // !!!
+  const url = urlBase + queryParams;
+
+  request(url, (error, response, body) => {
+    console.log(`error: ${error}`);
+    console.log(`status code: ${response}` && response.statusCode);
+    console.log(`body: ${body.substring(0, 60)}`);
+    console.log(typeof body); // string
+    const moviesObj = JSON.parse(body); // now: object
+
+    res.render('movies', { movies: moviesObj });
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('search');
 });
 
 

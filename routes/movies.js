10eec624a382947fcf122696266e59205baf71e0
darco2018 <newsecond@outlook.com>
2019-09-21 13:16:09 +0200

    Display movies from API

--- routes/movies.js
+++ routes/movies.js
@@ -1,17 +1,31 @@
+/* eslint-disable no-console */
 const express = require('express');
+const request = require('request');
 
 const router = express.Router();
+const OMDbAPIkey = '111ffc84';
+const urlBase = `http://www.omdbapi.com/?apikey=${OMDbAPIkey}&`; // + 's=polish';
 
 function Movie(title, year) {
   this.title = title;
   this.year = year;
 }
-
-const inMemoryMovieDb = [new Movie('Film 1', '1923'), new Movie('Film 2', '1923'), new Movie('Film 3', '1923')];
+// const inMemoryMovieDb = [new Movie('Film 1', '1923'), new Movie('Film 2', '1923'), new Movie('Film 3', '1923')];
 
 router.get('/', (req, res) => {
-  res.render('movies', { movies: inMemoryMovieDb });
-  // res.send('movies route');
+  const queryParams = 's=polish'; // !!!
+  const url = urlBase + queryParams;
+
+  request(url, (error, response, body) => {
+    console.log(`error: ${error}`);
+    console.log(`status code: ${response}` && response.statusCode);
+    console.log(`body: ${body.substring(0, 60)}`);
+    console.log(typeof body); // string
+    const moviesObj = JSON.parse(body); // now: object
+
+    res.render('movies', { movies: moviesObj });
+  });
+  // res.render('movies', { movies: inMemoryMovieDb });
 });
 
 
